65ed8e9df5eae98ef3c7a427af1fc0cf7e589480
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-08-29 16:05:14 -0600

    update schema and for parent id for response set for modifctions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709214402) do
+ActiveRecord::Schema.define(:version => 20130829212407) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -141,6 +141,8 @@ ActiveRecord::Schema.define(:version => 20130709214402) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.string   "api_id"
+    t.string   "parenet_id"
+    t.string   "parent_id"
   end
 
   add_index "response_sets", ["access_code"], :name => "response_sets_ac_idx", :unique => true
@@ -244,8 +246,8 @@ ActiveRecord::Schema.define(:version => 20130709214402) do
   add_index "surveys", ["api_id"], :name => "uq_surveys_api_id", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => ""
+    t.string   "encrypted_password",     :default => ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -256,9 +258,10 @@ ActiveRecord::Schema.define(:version => 20130709214402) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username",               :default => "", :null => false
+    t.string   "name"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "validation_conditions", :force => true do |t|
